fbe359b6bb6746a235622299244d82321442b87b
CemOzgr <cemozgur9@hotmail.com>
2021-06-01 02:56:17 +0300

    fixed async timeout exception

--- src/main/java/com/bitirme/bitirmeapi/notification/sse/SseResource.java
+++ src/main/java/com/bitirme/bitirmeapi/notification/sse/SseResource.java
@@ -1,8 +1,6 @@
 package com.bitirme.bitirmeapi.notification.sse;
 
 import com.bitirme.bitirmeapi.security.MemberDetails;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -11,14 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 @RequestMapping("/api/sse")
 public class SseResource {
 
     private final SseService sseService;
-    private static final Logger LOGGER = LoggerFactory.getLogger(SseResource.class);
 
     @Autowired
     public SseResource(SseService sseService) {
--- src/main/java/com/bitirme/bitirmeapi/notification/sse/SseService.java
+++ src/main/java/com/bitirme/bitirmeapi/notification/sse/SseService.java
@@ -4,31 +4,44 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SseService {
+
+    private final ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(1);
 
     public SseEmitter subscribeAndGetEmitter(int memberId) {
         final long ONE_HOUR = 3600000L;
 
         SseEmitter emitter = new SseEmitter(ONE_HOUR);
-        sendInitEvent(emitter);
+        sendNamedEvent(emitter, "INIT");
 
         SseEmitterStore.emitters.put(memberId, emitter);
         emitter.onCompletion(() -> SseEmitterStore.emitters.remove(memberId, emitter));
         emitter.onTimeout(() -> SseEmitterStore.emitters.remove(memberId, emitter));
         emitter.onError(e -> SseEmitterStore.emitters.remove(memberId, emitter));
 
+        sendPingToAllEmitters();
+
         return emitter;
     }
 
-    private void sendInitEvent(SseEmitter emitter) {
+    private void sendNamedEvent(SseEmitter emitter, String name) {
         try {
-            emitter.send(SseEmitter.event().name("INIT"));
+            emitter.send(SseEmitter.event().name(name));
         } catch(IOException e) {
             e.printStackTrace();
             throw new RuntimeException("Could not establish connection");
         }
+    }
+
+    public void sendPingToAllEmitters() {
+        scheduledThreadPool.scheduleAtFixedRate(() ->
+                SseEmitterStore.emitters.values()
+                        .forEach(emitter -> sendNamedEvent(emitter, "PING")), 149, 149, TimeUnit.SECONDS);
     }
 
     public void sendNamedEventToMember(int memberId, Object eventObject, String eventName) {
